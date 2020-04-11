8c8f04b5832880957deaeace33298cb69a305dbc
Roland Fuhrer <roland@resterland.ch>
2020-04-11 12:47:04 +0200

    Setting back everthing

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,3 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3_MODE') || die();
-
-/***************
- * Add default RTE configuration
- */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['resterland_bootstrap'] = 'EXT:resterland_bootstrap/Configuration/RTE/Default.yaml';
-
-/***************
- * PageTS
- */
-ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/All.tsconfig">');
