99f116ce67b71accb8b2c1e1e9a262db1644b75f
Roland Fuhrer <roland@resterland.ch>
2020-04-14 10:01:26 +0200

    Base settings

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,3 +1,19 @@
 <?php
-
 defined('TYPO3_MODE') || die();
+
+call_user_func(function()
+{
+    /**
+     * Temporary variables
+     */
+    $extensionKey = 'resterland_bootstrap';
+
+    /**
+     * Default TypoScript for ResterlandBootstrap
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript',
+        'Resterland Bootstrap'
+    );
+});
