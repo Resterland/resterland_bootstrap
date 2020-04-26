6e2bdfee2f61412485b759b307f57475be2c7c05
Roland Fuhrer <roland@resterland.ch>
2020-04-26 10:49:27 +0200

    Updated everything

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,5 +1,8 @@
 <?php
+
 defined('TYPO3_MODE') || die();
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /**
  * Temporary variables
@@ -9,10 +12,33 @@ $extensionKey = 'resterland_bootstrap';
 /**
  * Default PageTS for ResterlandBootstrap
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+
+// BackendLayouts
+ExtensionManagementUtility::registerPageTSConfigFile(
     $extensionKey,
-    'Configuration/TsConfig/Main/All.tsconfig',
-    'Resterland Bootstrap'
+    'Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts.tsconfig',
+        'Resterland Bootstrap: Backend Layouts'
+);
+
+// TCEMAIN
+ExtensionManagementUtility::registerPageTSConfigFile(
+    $extensionKey,
+    'Configuration/TsConfig/Page/TCEMAIN.tsconfig',
+    'Resterland Bootstrap: TCEMAIN'
+);
+
+// TCEFORM
+ExtensionManagementUtility::registerPageTSConfigFile(
+    $extensionKey,
+    'Configuration/TsConfig/Page/TCEFORM.tsconfig',
+    'Resterland Bootstrap: TCEFORM'
+);
+
+// SHARED
+ExtensionManagementUtility::registerPageTSConfigFile(
+    $extensionKey,
+    'Configuration/TsConfig/Page/Mod/SHARED.tsconfig',
+    'Resterland Bootstrap: SHARED'
 );
 
 /**
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,27 @@
 <?php
+
 defined('TYPO3_MODE') || die();
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /***************
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['resterland_bootstrap'] = 'EXT:resterland_bootstrap/Configuration/RTE/Default.yaml';
 
-/***************
- * PageTS
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/All.tsconfig">');
+// Add BackendLayouts for the BackendLayout DataProvider
+ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts.tsconfig">');
+
+// SHARED
+ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/Mod/SHARED.tsconfig">');
+
+// RTE
+ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/RTE.tsconfig">');
+
+// TCEMAIN
+ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/TCEMAIN.tsconfig">');
+
+// TCEFORM
+ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:resterland_bootstrap/Configuration/TsConfig/Page/TCEFORM.tsconfig">');
 
 
